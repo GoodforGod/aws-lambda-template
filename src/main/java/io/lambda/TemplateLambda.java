530d43b74e386448e17b53b6ed463ee9183a4012
Anton Kurako <goodforgod.dev@gmail.com>
2021-03-22 21:30:52 +0300

    [0.9.0-SNAPSHOT] Annotation package fixed

--- src/main/java/io/lambda/TemplateLambda.java
+++ src/main/java/io/lambda/TemplateLambda.java
@@ -1,12 +1,10 @@
 package io.lambda;
 
 import io.aws.lambda.runtime.Lambda;
-import io.aws.lambda.runtime.model.AwsGatewayRequest;
 import io.micronaut.core.annotation.TypeHint;
 import org.jetbrains.annotations.NotNull;
 
 import javax.inject.Singleton;
-import java.util.Map;
 import java.util.UUID;
 
 @TypeHint({Book.class, User.class})
